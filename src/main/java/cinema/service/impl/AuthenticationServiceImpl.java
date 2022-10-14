43d7e1104a440cc4fc6ca7c501b0c32a10cb7e8c
Heorhiy Nezbrytskiy <georgiy.nezbritskiy@gmail.com>
2022-10-14 10:13:44 +0300

    configure Spring, add models, add controller, dao, service layers, add custom annotations

---
+++ src/main/java/cinema/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,39 @@
+package cinema.service.impl;
+
+import cinema.model.Role;
+import cinema.model.User;
+import cinema.service.AuthenticationService;
+import cinema.service.RoleService;
+import cinema.service.ShoppingCartService;
+import cinema.service.UserService;
+import java.util.Set;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final String DEFAULT_ROLE = "USER";
+    private final UserService userService;
+    private final ShoppingCartService shoppingCartService;
+    private final RoleService roleService;
+
+    public AuthenticationServiceImpl(UserService userService,
+                                     ShoppingCartService shoppingCartService,
+                                     RoleService roleService, PasswordEncoder encoder) {
+        this.userService = userService;
+        this.shoppingCartService = shoppingCartService;
+        this.roleService = roleService;
+    }
+
+    @Override
+    public User register(String email, String password) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(user.getPassword());
+        Role role = roleService.getByName(DEFAULT_ROLE);
+        user.setRoles(Set.of(role));
+        userService.add(user);
+        shoppingCartService.registerNewShoppingCart(user);
+        return user;
+    }
+}
